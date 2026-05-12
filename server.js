bdb77c0b409d740ae14d00f5a125ba3e393e04d7
ahmadkabir0702 <ahmad.kabir0702@gmail.com>
2026-05-12 07:36:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const { google } = require('googleapis');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Add this ONE line right here!
 app.use(express.static('public'));
 
 const express = require('express');
